3fc82a96b64e9f784faa188bbc5a1453d5c5edd3
Li541001 <changtibo@gmail.com>
2023-04-08 19:25:09 +0800

    test添加

--- src/commands/test/index.js
+++ src/commands/test/index.js
@@ -1,13 +1,8 @@
 import { SlashCommandBuilder } from "discord.js";
-import { test } from "./test.js";
-
 
 export const command = new SlashCommandBuilder()
   .setName("test")
   .setDescription("測試用")
-
-
-
 
 export const action = async(ctx)=>{
   await ctx.reply("閉嘴")
